f149d99fa5e5541c8527baa92313e08bc9a041af
ocilical <ocilical@gmail.com>
2020-04-02 17:47:39 -0400

    Added ability to change brush size

--- draw.js
+++ draw.js
@@ -1,9 +1,14 @@
 const canvas = document.getElementById('coolCanvas');
 const ctx = canvas.getContext('2d');
 
+ctx.lineCap = 'round';
+
 let mouseDown = false;
 let x = 0;
 let y = 0;
+let size = 1;
+
+const clamp = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b)); //clamp num to range a,b
 
 function startDraw() {
     mouseDown = true;
@@ -28,7 +33,15 @@ function drawLine() {
     }
 }
 
+function changeSize() {
+    console.log(event.deltaY);
+    size = clamp(size - Math.sign(event.deltaY), 1, 72);
+    ctx.lineWidth = size;
+    document.getElementById("sizeText").innerHTML = 'size: ' + size;
+}
+
 canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mouseup', stopDraw);
 canvas.addEventListener('mouseout', stopDraw);
 canvas.addEventListener('mousemove', drawLine);
+canvas.addEventListener('wheel', changeSize);
